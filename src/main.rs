8c0c9dc3483e07c8c815570f240d7ffb459886f8
Pipeto84 <pipejarauribeboteroa@gmail.com>
2023-10-06 12:28:19 -0500

    window with green and blue rect

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate sdl2;
-use std::{time::Duration,thread::sleep};
-use sdl2::{pixels::Color,event::Event,keyboard::Keycode,render::{Texture,TextureCreator},
-    rect::Rect};
+use std::{time::{Duration,SystemTime},thread::sleep};
+use sdl2::{pixels::Color,event::Event,keyboard::Keycode,render::{Texture,TextureCreator, Canvas},
+    rect::Rect,video::{Window,WindowContext}};
 const TEXTURE_SIZE:u32=32;
 pub fn main() {
     let sdl_context=sdl2::init().expect("SDL initialization failed");
@@ -17,13 +17,11 @@ pub fn main() {
         .build()
         .expect("couldn't get window's canvas");
     let texture_creator:TextureCreator<_>=canvas.texture_creator();
-    let mut square_texture:Texture=texture_creator
-        .create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
+    let green_square=create_texture_rect(&mut canvas, &texture_creator, TextureColor::Green, TEXTURE_SIZE)
         .expect("failed to create a texture");
-    canvas.with_texture_canvas(&mut square_texture, |texture|{
-        texture.set_draw_color(Color::RGB(0, 255, 0));
-        texture.clear();
-    }).expect("faliled to color a texture");
+    let blue_square=create_texture_rect(&mut canvas, &texture_creator, TextureColor::Blue, TEXTURE_SIZE)
+        .expect("failed to create a texture");
+    let timer=SystemTime::now();
     let mut event_pump=sdl_context.event_pump().expect("failed to get SDL event pump");
 
     'running: loop {
@@ -36,11 +34,39 @@ pub fn main() {
         }
         canvas.set_draw_color(Color::RGB(255, 0, 0));
         canvas.clear();
+        let display_green=match timer.elapsed() {
+            Ok(elapsed)=>elapsed.as_secs()%2==0,
+            Err(_)=>{true}
+        };
+        let square_texture=if display_green {
+            &green_square
+        } else{
+            &blue_square
+        };
         canvas.copy(&square_texture, 
             None, 
             Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE))
             .expect("couldn't copy texture into window");
         canvas.present();
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+#[derive(Clone, Copy)]
+enum TextureColor {
+    Green,Blue,
+}
+fn create_texture_rect<'a>(canvas:&mut Canvas<Window>,texture_creator:&'a TextureCreator<WindowContext>,
+        color:TextureColor,size:u32)->Option<Texture<'a>> {
+    if let Ok(mut square_texture) = texture_creator.create_texture_target(None, size, size) {
+        canvas.with_texture_canvas(&mut square_texture, |texture|{
+            match color {
+                TextureColor::Green=>texture.set_draw_color(Color::RGB(0, 255, 0)),
+                TextureColor::Blue=>texture.set_draw_color(Color::RGB(0, 0, 255)),
+            }
+            texture.clear();
+        }).expect("failed to color a texture");
+        Some(square_texture)
+    }else {
+        None
     }
 }
